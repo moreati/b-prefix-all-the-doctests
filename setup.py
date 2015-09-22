551506da3702228999dfd1c25c505b59d3ac81c7
Alex Willmer <alex@moreati.org.uk>
2015-09-22 17:16:32 +0100

    Add package metadata, choose Apache License

--- setup.py
+++ setup.py
@@ -1,7 +1,45 @@
 from setuptools import setup
 
+import io
+import os
+
+def read(fname, encoding='utf-8'):
+    here = os.path.dirname(__file__)
+    with io.open(os.path.join(here, fname), encoding=encoding) as f:
+        return f.read()
+
 setup(
     name='pretext',
     version='0',
+
+    description='Use doctest with bytes, str & unicode on Python 2.x and 3.x',
+    long_description=read('README.rst'),
+    url='https://github.com/moreati/b-prefix-all-the-doctests',
+
+    author='Alex Willmer',
+    author_email='alex@moreati.org.uk',
+
+    license='Apache Software License 2.0',
+
     py_modules=['pretext'],
+
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Apache Software License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Programming Language :: Python :: Implementation :: PyPy',
+        'Topic :: Documentation',
+        'Topic :: Software Development :: Libraries',
+        'Topic :: Software Development :: Testing',
+    ],
+    keywords='doctest bytes unicode bytestring prefix literal string str',
 )
