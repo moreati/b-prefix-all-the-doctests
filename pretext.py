1aa5748d78b1047958c24bd2cce4a69da543c3d6
Alex Willmer <alex@moreati.org.uk>
2015-09-24 16:25:19 +0100

    Add Bytesrepr/UnicodeRepr for 3.x/2.x behaviour

--- pretext.py
+++ pretext.py
@@ -10,6 +10,8 @@ import sys
 __all__ = [
     'Repr',
     'PrefixRepr',
+    'BytesRepr',
+    'UnicodeRepr',
 ]
 
 
@@ -64,4 +66,55 @@ class PrefixRepr(Repr):
         else:
             # Python 3.x
             return u'u%s' % (self._old_repr(obj),)
+
+
+class BytesRepr(PrefixRepr):
+    """A Repr that prefixes byte strings, and doesn't truncate
+
+    This mirrors the behaviour of repr() in Python 3.x.
+
+    >>> py3_repr = BytesRepr().repr
+    >>> py3_repr(b'0123456789')
+    "b'0123456789'"
+    >>> py3_repr(u'0123456789')
+    "'0123456789'"
+    """
+
+    def repr_str(self, obj, level):
+        if str is bytes:
+            # Python 2.x
+            return b'b%s' % (self._old_repr(obj),)
+        else:
+            # Python 3.x
+            return self._old_repr(obj)
+
+    def repr_unicode(self, obj, level):
+        # Only Python 2.x has a builtin type called unicode
+        r = self._old_repr(obj)
+        assert r[0] == 'u'
+        return r[1:]
+
+
+class UnicodeRepr(PrefixRepr):
+    """A Repr that prefixes textual (unicode) strings & doesn't truncate
+
+    This mirrors the behaviour of repr() in Python 2.x.
+
+    >>> py2_repr = UnicodeRepr().repr
+    >>> py2_repr(b'0123456789')
+    "'0123456789'"
+    >>> py2_repr(u'0123456789')
+    "u'0123456789'"
+    """
+
+    def repr_str(self, obj, level):
+        if str is bytes:
+            return self._old_repr(obj)
+        else:
+            return u'u%s' % (self._old_repr(obj),)
+
+    def repr_bytes(self, obj, level):
+        r = self._old_repr(obj)
+        assert r[0] == 'b'
+        return r[1:]
 
