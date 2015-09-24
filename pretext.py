b987bc90a0a368675c8ac25d0e4d05631adc9758
Alex Willmer <alex@moreati.org.uk>
2015-09-24 16:22:42 +0100

    Refactor truncation removal out of PrefixRepr

--- pretext.py
+++ pretext.py
@@ -8,16 +8,23 @@ except ImportError:
 import sys
 
 __all__ = [
+    'Repr',
     'PrefixRepr',
 ]
 
 
-class PrefixRepr(reprlib.Repr):
-    """A Repr variant that always prefixes bytes/str/unicode literals
+class Repr(reprlib.Repr):
+    """A Repr that doesn't truncate it's output
+
+    >>> myrepr = Repr().repr
+    >>> myrepr('0123456789')
+    "'0123456789'"
+    >>> len(myrepr('0123456789'))
+    12
+    >>> len(myrepr('0123456789'*1000))
+    10002
     """
     def __init__(self):
-        self._old_repr = repr
-
         self.maxlevel = sys.maxsize
         self.maxdict = sys.maxsize
         self.maxlist = sys.maxsize
@@ -28,6 +35,24 @@ class PrefixRepr(reprlib.Repr):
         self.maxlong = sys.maxsize
         self.maxstring = sys.maxsize
         self.maxother = sys.maxsize
+
+
+class PrefixRepr(Repr):
+    """A Repr that always prefixes any string, and doesn't truncate
+
+    For byte strings this mirrors the behaviour of repr() in Python 3.x.
+    For textual strings this mirrors the behaviour of repr() in Python 2.x.
+
+    >>> prefix_repr = PrefixRepr().repr
+    >>> prefix_repr(b'0123456789')
+    "b'0123456789'"
+    >>> prefix_repr(u'0123456789')
+    "u'0123456789'"
+    """
+
+    def __init__(self):
+        Repr.__init__(self)
+        self._old_repr = repr
 
     def repr_str(self, obj, level):
         """Return the repr of a string, prefixed with b or u
